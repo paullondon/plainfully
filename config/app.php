bbb3ff2f4409f906fd2fefe1e7accc7bfe03f4f8
paul londom <paul.london@me.com>
2025-12-01 13:32:31 +0000

    A3 Css adjustments

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     'app' => [
         'base_url' => getenv('APP_BASE_URL') ?: 'https://plainfully.com',
         'env'      => getenv('APP_ENV') ?: 'local', // 'Live' is fine, we just treat it as a label
-        'css' => '011225-004'
+        'css' => '011225-005'
     ],
 
     // Database connection (MariaDB / MySQL)
