b4eee51a9512407bb2e28ff68fbb1cd0da36e570
paul londom <paul.london@me.com>
2025-12-01 13:08:47 +0000

    A3 Forgot CSS changes

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     'app' => [
         'base_url' => getenv('APP_BASE_URL') ?: 'https://plainfully.com',
         'env'      => getenv('APP_ENV') ?: 'local', // 'Live' is fine, we just treat it as a label
-        'css' => '011225-001'
+        'css' => '011225-002'
     ],
 
     // Database connection (MariaDB / MySQL)
