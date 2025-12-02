cfb94210977121ff0237d74ff25b025b84f23348
paul londom <paul.london@me.com>
2025-12-02 22:31:24 +0000

    A3-18

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     'app' => [
         'base_url' => getenv('APP_BASE_URL') ?: 'https://plainfully.com',
         'env'      => getenv('APP_ENV') ?: 'local', // 'Live' is fine, we just treat it as a label
-        'css' => 'A3-017'
+        'css' => 'A3-018'
     ],
 
     // Database connection (MariaDB / MySQL)
